f9df213ee4a08334f39208b12bf62f344f794e1c
kkcty <jdycty2002@163.com>
2025-03-14 17:26:48 +0800

    基本跑通的 test_list_page

--- emag_stock_monitor/models.py
+++ emag_stock_monitor/models.py
@@ -1,6 +1,6 @@
 """数据模型"""
 
-from typing import Iterable, Iterator, Optional, TypedDict, Self
+from typing import Optional, Self, TypedDict
 
 from scraper_utils.utils.emag_util import build_product_url, validate_pnk
 
@@ -13,7 +13,7 @@ class _ProductTypedDict(TypedDict):
     url: str
 
 
-class ListPageProduct:
+class Product:
     """
     产品列表页上的一个产品
 
@@ -21,7 +21,7 @@ class ListPageProduct:
 
     * `pnk`
     * `source_url`: 该产品来自哪个产品列表页
-    * `rank`: 该产品在其来源产品列表页上的序号（从 1 开始）
+    * `rank`: 该产品在其来源产品列表页上的排序（从 1 开始）
     * `qty`: 产品最大可加购数（`None` 或正整数）
     * `url`: 产品详情页链接
 
@@ -33,20 +33,16 @@ class ListPageProduct:
     """
 
     def __init__(self, pnk: str, source_url: str, rank: int, qty: Optional[int] = None) -> None:
-        self.pnk = pnk
+        if not validate_pnk(pnk=pnk):
+            raise ValueError(f'"{pnk}" 不符合 pnk 规则')
+        self.__pnk = pnk
         self.source_url = source_url
         self.rank = rank
         self.qty = qty
 
     @property
     def pnk(self) -> str:
-        return self._pnk
-
-    @pnk.setter
-    def pnk(self, pnk: str) -> None:
-        if not validate_pnk(pnk=pnk):
-            raise ValueError(f'"{pnk}" 不符合 pnk 规则')
-        self._pnk = pnk
+        return self.__pnk
 
     @property
     def rank(self) -> int:
@@ -70,93 +66,29 @@ class ListPageProduct:
 
     @property
     def url(self) -> str:
-        return build_product_url(pnk=self._pnk)
+        return build_product_url(pnk=self.__pnk)
 
     def __eq__(self, other) -> bool:
         return (
             isinstance(other, self.__class__)
-            and self._pnk == other._pnk
+            and self.__pnk == other.__pnk
             and self.source_url == other.source_url
         )
 
     def __hash__(self) -> int:
-        return hash((self._pnk, self.source_url))
+        return hash((self.__pnk, self.source_url))
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}(pnk="{self._pnk}", origin_url="{self.source_url}", qty={self.qty}, rank={self.rank})'
+        return f'{self.__class__.__name__}(pnk="{self.__pnk}", origin_url="{self.source_url}", qty={self.qty}, rank={self.rank})'
 
     def as_dict(self) -> _ProductTypedDict:
         return {
-            'pnk': self._pnk,
+            'pnk': self.__pnk,
             'source_url': self.source_url,
             'rank': self.rank,
             'qty': self.qty,
             'url': self.url,
         }
 
-
-class Products:
-    """产品集合"""
-
-    # FIXME 需要添加一个修改 qty 的功能
-
-    def __init__(self, products: Optional[Iterable[ListPageProduct]] = None) -> None:
-        if products is None:
-            products = list()
-        self.__products = list(products)
-        self.__modified: bool = True
-
-    @property
-    def products(self) -> list[ListPageProduct]:
-        # 按照 source_url-rank 的顺序对产品进行排序，
-        # 顺便对于重复的产品，取其 rank 最小的保留，其余去除
-
-        if self.__modified is False:  # 如果 __products 没被修改那就直接返回 __products
-            return self.__products
-
-        temp = set(i for i in sorted(self.__products, key=lambda p: (p._pnk, p.source_url, p.rank)))
-        result = list(sorted(temp, key=lambda p: (p.source_url, p.rank)))
-        self.__products = result
-        self.__modified = False
-        return self.__products
-
-    def append(self, value: ListPageProduct) -> None:
-        """添加一个元素"""
-        self.__products.append(value)
-        self.__modified = True
-
-    def appends(self, *values: ListPageProduct) -> None:
-        """添加多个元素"""
-        for v in values:
-            self.append(v)
-
-    def have_pnk(self, pnk: str) -> bool:
-        """检测产品列表中有无该 pnk"""
-        return validate_pnk(pnk=pnk) and any(p._pnk == pnk for p in self)
-
-    def __add__(self, other) -> Self:
-        if not isinstance(other, self.__class__):
-            return NotImplemented
-        result = self.__class__(products=self.products + other.products)
-        return result
-
-    def __iadd__(self, other) -> Self:
-        if not isinstance(other, self.__class__):
-            return NotImplemented
-        sp = self.products
-        op = other.products
-        self.__products = sp + op
-        self.__modified = True
-        return self
-
-    def __iter__(self) -> Iterator[ListPageProduct]:
-        return iter(self.products)
-
-    def __contains__(self, value: ListPageProduct) -> bool:
-        return value in self.products
-
-    def __len__(self) -> int:
-        return len(self.products)
-
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}(count="{len(self)}")'
+    def __copy__(self) -> Self:
+        return self.__class__(pnk=self.pnk, source_url=self.source_url, rank=self.rank, qty=self.qty)
--- test_list_page.py
+++ test_list_page.py
@@ -1,17 +1,19 @@
 import asyncio
+import csv
 from pathlib import Path
 
 from scraper_utils.utils.browser_util import BrowserManager, ResourceType, MS1000
-from scraper_utils.utils.json_util import write_json
+from scraper_utils.utils.file_util import read_file
 
 from emag_stock_monitor.logger import logger
-from emag_stock_monitor.page_handlers.list_page import add_to_cart, wait_page_load
+from emag_stock_monitor.page_handlers.list_page import handle_list_page, wait_page_load
 
 
 CWD = Path.cwd()
 
 
 async def main():
+    hide_cookie_banner_js = read_file(CWD.joinpath('js/hide-cookie-banner.js'), mode='str', async_mode=False)
     async with BrowserManager(
         executable_path='C:/Program Files/Google/Chrome/Application/chrome.exe',
         channel='chrome',
@@ -25,15 +27,32 @@ async def main():
             default_timeout=60 * MS1000,
         )
         page = await context.new_page()
-        await page.goto('https://www.emag.ro/jocuri-societate/c')
+        await page.add_init_script(hide_cookie_banner_js)
+
+        # await page.goto('https://www.emag.ro/jocuri-societate/c')
+        # await page.goto('https://www.emag.ro/jocuri-societate/p2/c')
+        # await page.goto('https://www.emag.ro/jocuri-societate/p3/c')
+        # await page.goto('https://www.emag.ro/jocuri-societate/p4/c')
+        # await page.goto('https://www.emag.ro/jocuri-societate/p5/c')
+
+        # await page.goto('https://www.emag.ro/accesorii-fitness/c')
+        # await page.goto('https://www.emag.ro/accesorii-fitness/p2/c')
+        # await page.goto('https://www.emag.ro/accesorii-fitness/p3/c')
+        # await page.goto('https://www.emag.ro/accesorii-fitness/p4/c')
+        # await page.goto('https://www.emag.ro/accesorii-fitness/p5/c')
+
+        await page.goto('https://www.emag.ro/navomodele/c')
         await wait_page_load(page)
-        result = await add_to_cart(page)
-        result_save_path = write_json(
-            'result.json',
-            data=list(_.as_dict() for _ in result),
-            async_mode=False,
-            indent=4,
-        )
+        result = await handle_list_page(page)
+
+        # 保存成 csv 文件
+        result_save_path = 'result.csv'
+        with open(result_save_path, 'w', newline='', encoding='utf-8') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=['pnk', 'url', 'source_url', 'rank', 'qty'])
+            writer.writeheader()
+            for p in result:
+                writer.writerow(p.as_dict())
+
         logger.success(f'程序结束，爬取结果保存至 "{result_save_path}"')
 
 
